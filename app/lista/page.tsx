7d535c0f78abd2f0632624c2b4262d124064bf6b
sajuoiz <sam.juarez.ortiz@gmail.com>
2024-04-18 20:46:34 -0600

    terceros_cambios

--- app/lista/page.tsx
+++ app/lista/page.tsx
@@ -1,21 +1,24 @@
 import React from 'react';
 import Header from '../header';
 
+
+
 const products = [
-  { id: 1, title: 'Producto 1', image: '/product1.jpg' },
-  { id: 2, title: 'Producto 2', image: '/product2.jpg' },
-  { id: 3, title: 'Producto 3 ', image: '/product3.jpg' },
+  { id: 1, title: 'Audífonos Precio: 399 EUR', image: './imagenes/product1.jpg', width: 300, height: 200 },
+  { id: 2, title: 'Iphone Precio 500 EUR', image: './imagenes/product2.jpg', width: 180, height: 200 },
+  { id: 3, title: 'Tablet Precio 250 EUR', image: './imagenes/product3.jpg', width: 200, height: 200 },
   // Puedes agregar más productos aquí
 ];
 
 const IndexPage = () => {
   return (
     <div>
+      <Header />
       <h1>Lista de Productos</h1>
       <div className="products">
         {products.map(product => (
           <div key={product.id} className="product">
-            <img src={product.image} alt={product.title} />
+            <img src={product.image} alt={product.title} style={{ width: product.width, height: product.height }} />
             <h3>{product.title}</h3>
           </div>
         ))}
@@ -25,5 +28,4 @@ const IndexPage = () => {
 };
 
 export default IndexPage;
-
 
